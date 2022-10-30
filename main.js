a705e141b99817fb429c3cd542a0dcdfc64c9391
GurnikVitaliy <111244081+GurnikVitaliy@users.noreply.github.com>
2022-10-30 14:10:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ function data() {
     let l = document.querySelector('.login').value,
         p = document.querySelector('.pswd').value,
         out = document.querySelector('.out'),
+        login = document.createElement('li'),
+        password = document.createElement('li'),
         look = document.querySelector('.create-btn');
-        
-        look.addEventListener('click', () => {
-            for(let key in user) {
-                out.innerHTML += user[key]+'<br>';
-            }
-        })
 
         const user = {
-            login : l,
-            password : p
+            login : `login - ${l}`,
+            password : `password -${p}`
         }
+
+        look.addEventListener('click', () => {
+            for(let key in user) {
+                out.innerHTML += user[key] + '<br>';
+            }        
+        })
 }
 
 document.getElementById('btn').onclick = data;
